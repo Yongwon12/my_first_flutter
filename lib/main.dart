062007e3cacfad256fae363b256bce8ca61e024c
gim-yong-won <tjaaj159@naver.com>
2023-05-23 08:23:47 +0900

    클릭하여 링크로 이동

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-
-    Future.delayed(const Duration(seconds: 4), () {
-      setState(() {
-        isLoading = false;
-      });
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,37 +14,52 @@ class _MyAppState extends State<MyApp> {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.grey,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: isLoading
-          ? const Scaffold(
-        body: Center(
-          child: SpinKitFadingCircle(
-            color: Colors.grey,
-            size: 50.0,
-          ),
-        ),
-      )
-          : Scaffold(
-        appBar: AppBar(
-          title: const Text('플러터 테스트'),
-        ),
-        body: ListView(
-          children: const [
-            ListTile(
-              title: Text('Post 1'),
-              subtitle: Text('This is the content of post 1.'),
-            ),
-            ListTile(
-              title: Text('Post 2'),
-              subtitle: Text('This is the content of post 2.'),
-            ),
-            ListTile(
-              title: Text('Post 3'),
-              subtitle: Text('This is the content of post 3.'),
+      home: const ListPage(),
+    );
+  }
+}
+
+class ListPage extends StatelessWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('유튜브음악 링크페이지'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Card(
+              child: ListTile(
+                leading: Image.asset(
+                  '/Users/gim-yong-won/Desktop/my_first_flutter/image/ballerino.jpeg', // Image path for YouTube logo
+                  width: 48,
+                  height: 48,
+                ),
+                title: const Text('Ballerino - MR'),
+                subtitle: const Text('눌러서 이동'),
+                onTap: () {
+                  _launchURL(Uri.parse('https://www.youtube.com/watch?v=6cDlodq4fiM'));
+                },
+              ),
             ),
           ],
         ),
+
       ),
     );
+  }
+
+  void _launchURL(Uri url) async {
+    if (await canLaunch(url.toString())) {
+      await launch(url.toString());
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }

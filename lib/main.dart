15b39edc34334e0e2e836f8c2f16c0a3106f596e
gim-yong-won <tjaaj159@naver.com>
2023-06-10 23:54:37 +0900

    악

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyHomePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             ElevatedButton(
-              child: const Text('최애 음악'),
+              child: const Text('즐겨듣는 음악'),
               onPressed: () {
                 Navigator.push(
                   context,
